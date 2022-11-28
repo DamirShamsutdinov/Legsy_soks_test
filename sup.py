b733de2000ebb190f6cd30338c16f7c8c58dc451
Дамир Шамсутдинов <d4mirshd@yandex.ru>
2022-11-28 17:30:24 +0600

    pars done completely

--- sup.py
+++ sup.py
@@ -1,2 +1,9 @@
+import json
+from pprint import pprint
 
-
+with open('catch_product.json', 'r', encoding='utf8') as file:
+    data = json.load(file)
+for i in data:
+    print(i)
+print()
+pprint(data)
