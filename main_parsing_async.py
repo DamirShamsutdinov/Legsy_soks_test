6c58c0121e22927e62f5e975a134ce769f70e461
Дамир Шамсутдинов <d4mirshd@yandex.ru>
2022-12-02 19:52:01 +0600

    Не до конца разобрался с state: FSMContext Но в принципе робит

--- main_parsing_async.py
+++ main_parsing_async.py
@@ -1,24 +1,31 @@
 import asyncio
 import json
-import time
+import logging
 
 import aiohttp
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='main.log',
+    filemode='w',
+    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s'
+)
 
 
 async def get_url(pages):
     headers = {
-        'Accept': '*/*',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36',
+        "Accept": "*/*",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
     }
     search_list = []
     async with aiohttp.ClientSession() as session:
         for page in pages:
             async with session.get(
                     f"https://catalog.wb.ru/catalog/men_clothes2/catalog?appType=1&couponsGeo=12,3,18,15,21&curr=rub&dest=-1029256,-102269,-2162196,-1257786&emp=0&kind=1&lang=ru&locale=ru&page={page}&pricemarginCoeff=1.0&reg=1&regions=80,64,83,4,38,33,70,82,69,68,86,75,30,40,48,1,22,66,31,71&sort=popular&spp=28&sppFixGeo=4&subject=177;284;4853",
-                    headers=headers
+                    headers=headers,
             ) as response:
                 r = await response.json(content_type=None)
-                search_list.append(r['data']['products'])
+                search_list.append(r["data"]["products"])
         return search_list
 
 
@@ -30,22 +37,34 @@ async def get_data(articuls_list):
     for sub_item in products:
         for item in sub_item:
             for articul in articuls_list:
-                if item['id'] == articul:
-                    products_list.append({
-                        'pr_articul': item['id'],
-                        'pr_name': item['name'],
-                        'pr_page': count,
-                    })
+                if item["id"] == articul:
+                    products_list.append(
+                        {
+                            "pr_articul": item["id"],
+                            "pr_name": item["name"],
+                            "pr_page": count,
+                        }
+                    )
         count += 1
-    with open('result_data.json', mode='w', encoding='utf8') as f:
+    with open("result_data.json", mode="w", encoding="utf8") as f:
         json.dump(products_list, f, indent=4, ensure_ascii=False)
 
-"""ПРОВЕРКА async парсинга - прошла успешна"""
-a_list = [86210392, 39408901, 100749785, 43127482, 44325545, 64746739, 54673003, 74746118]
-asyncio.get_event_loop().run_until_complete(get_data(articuls_list=a_list))
+
+# """ПРОВЕРКА async парсинга - прошла успешна"""
+# a_list = [
+#     86210392,
+#     39408901,
+#     100749785,
+#     43127482,
+#     44325545,
+#     64746739,
+#     54673003,
+#     74746118,
+# ]
+# asyncio.get_event_loop().run_until_complete(get_data(articuls_list=a_list))
 
 
-# input_str = '86210392 39408901 100749785 43127482 44325545 64746739 54673003 74746118'
+# input_str = '39408901 100749785 43127482 64746739 54673003 74746118'
 # list1 = [int(i) for i in input_str.split()]
 # async def collect_data(add_list: list) -> json:
 #     await get_data(add_list)
--- main_tg_bot.py
+++ main_tg_bot.py
@@ -1,42 +1,69 @@
 import json
+import os
 
 from aiogram import Bot, Dispatcher, executor, types
-
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from main_parsing_async import get_data
+from dotenv import load_dotenv
 
-TOKEN = "5965362552:AAFiQZESJXjKaOmClto7jRQf9hB_k2a_cCc"
+load_dotenv()
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
+bot = Bot(token=os.getenv("TOKEN"))
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+start_buttons = ['Москва']
+keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+keyboard.add(*start_buttons)
 
 
-@dp.message_handler(commands="start")
-async def start_message(message: types.Message):
+class SearchParameters(StatesGroup):
+    articul = State()
+
+
+@dp.message_handler(commands=["start"])
+async def command_start(message: types.Message):
     await message.answer(
-        "Введите артикул товара или несколько (чз пробел)",
+        'Привет! Выбери город',
+        reply_markup=keyboard
     )
 
 
-@dp.message_handler()
-async def post_data(message: types.Message):
+@dp.message_handler(Text(equals='Москва'), state="*")
+async def command_start(message: types.Message, state: FSMContext):
+    await message.answer(
+        'Введи артикул товара или несколько (чз пробел)',
+    )
+    await state.set_state(SearchParameters.articul.state)
+
+
+
+@dp.message_handler(state=SearchParameters.articul)
+async def post_data(message: types.Message, state: FSMContext):
+    # products = await state.get_data(message.text)
+    # print(products)
     try:
         list_com = [int(i) for i in message.text.split()]
         await get_data(articuls_list=list_com)
+        # list_com = [int(i) for i in products]
+        # await get_data(articuls_list=list_com)
     except:
-        await message.answer(f"Данные введены некорректно, попробуйте еще раз!\n"
-                             f"Либо обратитесь к разработчикам.")
+        await message.answer(
+            f"Данные введены некорректно, попробуйте еще раз!\n"
+            f"Либо обратитесь к разработчикам."
+        )
     else:
-        print('Very good')
-        with open("result_data.json", encoding='utf8') as file:
+        # print("Very good")
+        with open("result_data.json", encoding="utf8") as file:
             data = json.load(file)
         for item in data:
-            card = f'{item["pr_articul"]} - {item["pr_name"]} - {item["pr_page"]}'
+            card = (
+                f"{item['pr_articul']} - " f"{item['pr_name']} - " f"{item['pr_page']}"
+            )
             await message.answer(card)
 
 
-def main():
-    executor.start_polling(dp)
-
-
 if __name__ == "__main__":
-    main()
+    executor.start_polling(dp, skip_updates=True)
